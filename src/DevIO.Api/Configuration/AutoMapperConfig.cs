b566e64d888b9c332c0a761290dd71937465a6e0
João Nascimento <joaonascimento@MacBook-Air-de-Joao.local>
2023-07-10 13:39:28 -0300

    AutoMapper ajustes configuracao

--- src/DevIO.Api/Configuration/AutoMapperConfig.cs
+++ src/DevIO.Api/Configuration/AutoMapperConfig.cs
@@ -5,12 +5,16 @@ using DevIO.Business.Models;
 namespace DevIO.Api.Configuration
 {
     public class AutoMapperConfig : Profile
-	{
-		public AutoMapperConfig()
-		{
-			CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
             CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
-            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+            CreateMap<ProdutoViewModel, Produto>();
+
+            CreateMap<Produto, ProdutoViewModel>()
+                .ForMember(dest => dest.NomeFornecedor,
+                opt => opt.MapFrom(src => src.Fornecedor.Nome));
 
         }
     }
